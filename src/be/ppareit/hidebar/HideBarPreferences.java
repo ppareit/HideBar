de36e8b867e6314353322c96747dcf7051f57a74
Pieter Pareit <pieter.pareit@gmail.com>
2011-09-18 15:28:46 +0200

    Added about information
    Forgot about about.xml

--- src/be/ppareit/hidebar/HideBarPreferences.java
+++ src/be/ppareit/hidebar/HideBarPreferences.java
@@ -4,23 +4,26 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Contributors:
  *     Pieter Pareit - initial API and implementation
  ******************************************************************************/
 package be.ppareit.hidebar;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.util.Log;
 
 public class HideBarPreferences extends PreferenceActivity {
@@ -35,11 +38,22 @@ public class HideBarPreferences extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
 
+        Preference aboutPreference = findPreference("about_preference");
+        aboutPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Dialog dlg = new Dialog(HideBarPreferences.this);
+                dlg.setContentView(R.layout.about);
+                dlg.setTitle(getResources().getText(R.string.about_label));
+                dlg.show();
+                return true;
+            }
+        });
+
         startService(new Intent(this, BackgroundService.class));
 
         Log.v(TAG, "onCreate'ed");
     }
-
 }
 
 
