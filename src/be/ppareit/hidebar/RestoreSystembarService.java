4d7f8732d9b58773ea220d4a1aec7ff9e748ba49
Pieter Pareit <pieter.pareit@gmail.com>
2012-06-27 18:34:34 +0200

    Made intercepting touch events device independent!

--- src/be/ppareit/hidebar/RestoreSystembarService.java
+++ src/be/ppareit/hidebar/RestoreSystembarService.java
@@ -18,22 +18,30 @@
  ******************************************************************************/
 package be.ppareit.hidebar;
 
+import java.util.LinkedList;
+
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.util.Log;
-import android.view.Display;
+import android.view.Gravity;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
-import be.ppareit.android.GlobalTouchListener;
+import android.view.WindowManager.LayoutParams;
+import android.widget.LinearLayout;
 
 public class RestoreSystembarService extends Service {
 
     static final String TAG = RestoreSystembarService.class.getSimpleName();
 
-    private GlobalTouchListener touchListener = null;
-    private GlobalTouchListener ghostBackButtonTouchListener = null;
+    private final LinkedList<View> mTouchAreas = new LinkedList<View>();
+
+    private long mBottomTouchTime = -47;
+    private long mTopTouchTime = 47;
 
     @Override
     public void onCreate() {
@@ -42,81 +50,120 @@ public class RestoreSystembarService extends Service {
         // start specialized listener, depending on how the systembar should show up
         switch (HideBarPreferences.methodToShowBar(this)) {
         case BOTTOM_TOUCH: {
-            // make a touch listener, on correct touch we show the statusbar and stop
-            touchListener = new GlobalTouchListener(this) {
-                @Override
-                public void onTouchEvent(MotionEvent event) {
-                    if (event.getAction() != MotionEvent.ACTION_DOWN) return;
-                    WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
-                    Display display = wm.getDefaultDisplay();
-                    if (event.getX() > 40 && event.getY() > display.getHeight() - 20) {
-                        Log.v(TAG, "Swipe Up detected");
-                        showSystembar();
-                        RestoreSystembarService.this.stopSelf();
+            WindowManager wm = (WindowManager)getSystemService(Context.WINDOW_SERVICE);
+            WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                    LayoutParams.MATCH_PARENT,
+                    LayoutParams.WRAP_CONTENT,
+                    LayoutParams.TYPE_SYSTEM_ALERT,
+                    LayoutParams.FLAG_NOT_FOCUSABLE|LayoutParams.FLAG_NOT_TOUCH_MODAL|
+                    LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                    PixelFormat.OPAQUE);
+            params.alpha = 0.0f;
+            params.height = 25;
+            params.gravity = Gravity.BOTTOM;
+            params.x = 0;
+            params.y = 0;
+            LinearLayout touchLayout = new LinearLayout(this);
+            touchLayout.setBackgroundColor(0x00FFFFFF);
+            touchLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+            if (HideBarPreferences.ghostbackButton(this)) {
+                View backArea = new View(this);
+                backArea.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        Log.d(TAG, "backArea touched");
+                        sendBackEvent();
+                        return false;
                     }
+                });
+                touchLayout.addView(backArea, 40, 25);
+            }
+            View restoreArea = new View(this);
+            restoreArea.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    Log.d(TAG, "touchArea touched");
+                    showSystembar();
+                    RestoreSystembarService.this.stopSelf();
+                    return false;
                 }
-            };
-            touchListener.startListening();
+            });
+            touchLayout.addView(restoreArea);
+            wm.addView(touchLayout, params);
+            mTouchAreas.add(touchLayout);
             break;
         }
         case BOTTOM_TOP_TOUCH: {
-            touchListener = new GlobalTouchListener(this) {
-                // as long as these two are initial different, 47 is just random number
-                long bottomTime = -47;
-                long topTime = 47;
-                @Override
-                public void onTouchEvent(MotionEvent event) {
-                    if (event.getAction() != MotionEvent.ACTION_DOWN) return;
-                    WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
-                    Display display = wm.getDefaultDisplay();
-                    if (event.getY() > display.getHeight() - 20) {
-                        bottomTime = event.getEventTime();
-                    } else if (event.getY() < 20) {
-                        topTime = event.getEventTime();
+            WindowManager wm = (WindowManager)getSystemService(Context.WINDOW_SERVICE);
+            WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                    LayoutParams.MATCH_PARENT,
+                    LayoutParams.WRAP_CONTENT,
+                    LayoutParams.TYPE_SYSTEM_ALERT,
+                    LayoutParams.FLAG_NOT_FOCUSABLE|LayoutParams.FLAG_NOT_TOUCH_MODAL|
+                    LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                    PixelFormat.OPAQUE);
+            params.alpha = 0.0f;
+            params.height = 25;
+            params.gravity = Gravity.BOTTOM;
+            params.x = 0;
+            params.y = 0;
+            LinearLayout bottomLayout = new LinearLayout(this);
+            bottomLayout.setBackgroundColor(0x00FFFFFF);
+            bottomLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+            if (HideBarPreferences.ghostbackButton(this)) {
+                View backArea = new View(this);
+                backArea.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        Log.d(TAG, "backArea touched");
+                        sendBackEvent();
+                        return false;
                     }
-                    if (Math.abs(bottomTime - topTime) < 5) {
-                        // top and bottom touch close in time
+                });
+                bottomLayout.addView(backArea, 40, 25);
+            }
+            View bottomArea = new View(this);
+            bottomArea.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    Log.d(TAG, "bottomArea touched");
+                    mBottomTouchTime = SystemClock.uptimeMillis();
+                    if (Math.abs(mBottomTouchTime - mTopTouchTime) < 2000) {
                         showSystembar();
                         RestoreSystembarService.this.stopSelf();
                     }
+                    return false;
                 }
-            };
-            touchListener.startListening();
+            });
+            bottomLayout.addView(bottomArea);
+            wm.addView(bottomLayout, params);
+            mTouchAreas.add(bottomLayout);
+
+            params.gravity = Gravity.TOP;
+            LinearLayout topLayout = new LinearLayout(this);
+            topLayout.setBackgroundColor(0x00FFFFFF);
+            topLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+            View topArea = new View(this);
+            topArea.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    Log.d(TAG, "topArea touched");
+                    mTopTouchTime = SystemClock.uptimeMillis();
+                    if (Math.abs(mBottomTouchTime - mTopTouchTime) < 2000) {
+                        showSystembar();
+                        RestoreSystembarService.this.stopSelf();
+                    }
+                    return false;
+                }
+            });
+            topLayout.addView(topArea);
+            wm.addView(topLayout, params);
+            mTouchAreas.add(topLayout);
             break;
 
         }
         case NONE:
             break;
-        }
-        if (HideBarPreferences.ghostbackButton(this)) {
-            ghostBackButtonTouchListener = new GlobalTouchListener(this) {
-                long lastTime = -1;
-                boolean ignoreOne = false;
-                @Override
-                public void onTouchEvent(MotionEvent event) {
-                    WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
-                    Display display = wm.getDefaultDisplay();
-                    long currentTime = SystemClock.uptimeMillis();
-                    // only do ghost back in left bottom corner
-                    if (event.getX() > 20 || event.getY() < display.getHeight() - 20)
-                        return;
-                    // only do a ghost back once every 2 seconds
-                    if (lastTime > currentTime - 2000)
-                        return;
-                    // BUG: java process buffers, so lot of events are  received much
-                    // to late, this ignores one event and this helps a lot
-                    if (ignoreOne) {
-                        lastTime = currentTime;
-                        ignoreOne = false;
-                        return;
-                    }
-                    Log.v(TAG, "Executing a ghost back event");
-                    lastTime = currentTime;
-                    ignoreOne = true;
-                    sendBackEvent();
-                }
-            };
-            ghostBackButtonTouchListener.startListening();
         }
 
     }
@@ -124,11 +171,9 @@ public class RestoreSystembarService extends Service {
     @Override
     public void onDestroy() {
         Log.v(TAG, "onDestroy");
-        touchListener.stopListening();
-        touchListener = null;
-        if (ghostBackButtonTouchListener != null) {
-            ghostBackButtonTouchListener.stopListening();
-            ghostBackButtonTouchListener = null;
+        WindowManager wm = (WindowManager)getSystemService(Context.WINDOW_SERVICE);
+        while (!mTouchAreas.isEmpty()) {
+            wm.removeView(mTouchAreas.pop());
         }
     }
 
